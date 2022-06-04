df92305f3047b873e01cb033f3dd4575c01c5aea
Khawaja Abaid Ullah <khawaja.abaid@gmail.com>
2022-06-04 22:19:51 +0500

    added some predict button functionality

--- prewordict.js
+++ prewordict.js
@@ -6,5 +6,22 @@ else {
 }
 
 function ready(){
-    document.getElementsByClassName()
+    document.getElementsByClassName("user-predict-button")[0].addEventListener('click',
+    predictButtonClicked)
+}
+
+function predictButtonClicked(){
+    console.log("predict button clicked")
+    imageElem = document.getElementsByClassName("cloud-image")[0]
+    img_parent = imageElem.parentElement
+    img_parent.removeChild(imageElem)
+    img_parent.innerHTML = `
+    <div class='result-image'>
+        <div>
+            <img src="images/peepoCheer.png" width=200px>
+        </div>
+    </div>`
+    parent_section = img_parent.parentElement.parentElement
+    h2_heading = parent_section.getElementsByTagName("h2")[0]
+    h2_heading.innerHTML = "Congratulations! You are a pre-wordict!"
 }
