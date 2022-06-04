4850cf1f45d5ffcd82f1a0811885e8855e73a58c
Khawaja Abaid Ullah <khawaja.abaid@gmail.com>
2022-06-04 21:28:52 +0500

    added relative links to navigation menu and working on the javascript part

---
+++ prewordict.js
@@ -0,0 +1,10 @@
+if (document.readyState == "loading") {
+    document.addEventListener("DOMContentLoaded", ready)
+}
+else {
+    ready()
+}
+
+function ready(){
+    document.getElementsByClassName()
+}
